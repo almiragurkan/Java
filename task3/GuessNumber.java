4ffd991b6c035d2e88a9a1595f187494d4a50a55
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-04 01:40:03 +0300

    Add files via upload

---
+++ task3/GuessNumber.java
@@ -0,0 +1,43 @@
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+
+	public static void main(String[] args) throws IOException {
+		Scanner reader = new Scanner(System.in); //Creates an object to read user input
+		Random rand = new Random(); //Creates an object from Random class
+		int number =rand.nextInt(100); //generates a number between 0 and 99
+
+
+		System.out.println("Hi! I'm thinking of a number between 0 and 99.");
+		System.out.print("Can you guess it: ");
+
+		int guess = reader.nextInt(); //Read the user input
+		int count = 1;
+
+		while (number != guess && guess != -1) {
+			System.out.println("Sorry!");
+			if (guess < number) {
+				System.out.println("Mine ise greater than your guess.");
+			}else{
+				System.out.println("Mine ise less than your guess.");
+			}
+			System.out.println("Type -1 to quit or guess another:");
+			guess = reader.nextInt();
+			count++;
+		}
+
+		if (number== guess) {
+			System.out.println("Congratulations! You won after " + count + " attemps");
+
+		}else {
+			System.out.println("Sorry, the number was " + number);
+		}
+
+		reader.close(); //Close the resource before exiting
+	}
+
+
+}
