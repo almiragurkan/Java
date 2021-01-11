e82149ee61d9cd6de7665156f145d1d8d10d09ee
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-11 23:47:05 +0300

    Add files via upload

---
+++ task9/shapes2d/Square.java
@@ -0,0 +1,18 @@
+package shapes2d;
+
+public class Square {
+
+	protected int side;
+
+	public Square(int side) {
+		this.side = side;
+	}
+	
+	public int area() {
+		return side * side;
+	}
+	
+	public String toString() {
+		return "side :" + side;
+	}
+}
