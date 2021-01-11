a380569683902bb41835626af36e79913c09d49b
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-11 23:43:06 +0300

    Add files via upload

---
+++ task9/almira/shapes/Circle.java
@@ -0,0 +1,13 @@
+package almira.shapes;
+
+public class Circle {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
